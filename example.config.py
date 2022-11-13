4f6f85ae7e446c430d056668a16ab5ba04a16428
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-13 13:09:39 -0600

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -4,5 +4,5 @@ token = 'DISCORD_BOT_TOKEN_HERE'
 # Your Discord Server ID Will Go Here 
 guild = 'SERVER_ID_HERE'
 
-# The Prefix You Want For Your Discord Bot
+# The Prefix You Want For Your Discord Bot, Keep in mind you will only be using slash commands. The Prefix is not needed.
 prefix = 'PREFIX_HERE'
