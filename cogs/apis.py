b0c94eb3b6e5b581724e5f3b0f38b54ca8710784
laserzz <82984448+laserzz@users.noreply.github.com>
2022-11-13 16:42:45 -0800

    fix api stuff

--- cogs/apis.py
+++ cogs/apis.py
@@ -12,7 +12,7 @@ import json
 class Apis(commands.Cog):
     
     def __init__(self, bot):
-    	self.bot = bot
+        self.bot = bot
         
     @commands.Cog.listener()
     async def on_ready(self):
@@ -21,6 +21,7 @@ class Apis(commands.Cog):
     # Image Genertor
     @commands.slash_command(name="generate", description="Generate an image from a prompt!")
     async def generate(inter, *, prompt: str):
+        await inter.response.defer()
         ETA = int(time.time() + 60)
         loading_images = ["loading/loading.gif",
                           "loading/loading2.gif",
@@ -30,10 +31,10 @@ class Apis(commands.Cog):
                           "loading/loading6.gif"]
         embed = disnake.Embed(title=f"Loading...! Generating image... ETA: <t:{ETA}:R>", color=disnake.Color.random())
         embed.set_image(file=disnake.File(random.choice(loading_images)))
-        msg = await inter.send(embed=embed)
+        await inter.send(embed=embed)
         async with aiohttp.request("POST", "https://backend.craiyon.com/generate", json={"prompt": prompt}) as resp:
             if resp.status != 200:
-                return await inter.send("An error occurred while generating the image.")
+                return await inter.edit_original_response(content="An error occurred while generating the image.")
             data = await resp.json()
             img = Image.open(BytesIO(base64.b64decode(data["images"][0])))
             img.save("image.png")
@@ -46,11 +47,12 @@ class Apis(commands.Cog):
     @commands.slash_command(name="bitcoin", description="Get the current price of Bitcoin!")
     async def bitcoin(inter):
         url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-        response = requests.get(url)
-        value = response.json()["bpi"]["USD"]["rate"]
-        embed = disnake.Embed(title=f"Bitcoin Price", description=f"Current Bitcoin Price: ${value}", color=disnake.Color.random())
-        embed.set_footer(text=f'Requested by {inter.author}', icon_url=inter.author.avatar.url)
-        await inter.send(embed=embed)
+        async with aiohttp.request("GET", url) as response:
+            text = await response.text()
+            value = json.loads(text)["bpi"]["USD"]["rate"]
+            embed = disnake.Embed(title=f"Bitcoin Price", description=f"Current Bitcoin Price: ${value}", color=disnake.Color.random())
+            embed.set_footer(text=f'Requested by {inter.author}', icon_url=inter.author.avatar.url)
+            await inter.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Apis(bot))
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,7 +7,7 @@ import config
 class moderation(commands.Cog):
     
     def __init__(self, bot):
-    	self.bot = bot
+        self.bot = bot
         
     @commands.Cog.listener()
     async def on_ready(self):
