0875c7d674396efc8ca9b7a9134d2dda362cb5b5
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-16 14:38:13 -0600

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,3 +1,10 @@
+###############################################
+#           Template made by Person0z         #
+#          https://github.com/Person0z        #
+#           Copyright© Person0z, 2022         #
+#           Do Not Remove This Header         #
+###############################################
+
 # Imports
 import disnake
 
