4d940e4f6bbbea734bcb945a8011bba133c96fb8
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-17 20:58:23 -0600

    Better Welcome & Good-Byes

--- cogs/general.py
+++ cogs/general.py
@@ -32,7 +32,7 @@ class general(commands.Cog):
     @commands.slash_command(name='help',
                             description='Get Useful Info About The Bot',)
     async def help(self, inter: disnake.ApplicationCommandInteraction):
-        embed = disnake.Embed(title="EvoGen's Personal Help Menu", color=disnake.Color.random())
+        embed = disnake.Embed(title="EvoGen's Personal Help Menu", color=config.Success())
         embed.set_author(name="Bot Info", icon_url=self.bot.user.avatar.url)
         embed.add_field(name="Birth Giver", value="Person0z#0812", inline=False)
         embed.add_field(name="Bot Version", value="v1.0", inline=False)
@@ -45,7 +45,7 @@ class general(commands.Cog):
     # Check Slash Command (Checks if the bot is online)
     @commands.slash_command(name="check", description="Check if the bot is online!")
     async def check(inter):
-        embed = disnake.Embed(title=f"Bot Status", description=f"Bot is online!", color=disnake.Color.random())
+        embed = disnake.Embed(title=f"Bot Status", description=f"Bot is online!", color=config.Success())
         embed.set_footer(text=f'Requested by {inter.author}', icon_url=inter.author.avatar.url)
         await inter.send(embed=embed)
 
@@ -53,7 +53,7 @@ class general(commands.Cog):
     @commands.slash_command(name='invite',
                             description='Get the invite link for the bot',)
     async def invite(self, inter: disnake.ApplicationCommandInteraction):
-        embed = disnake.Embed(title="EvoGen Invite URL", color=disnake.Color.random())
+        embed = disnake.Embed(title="EvoGen Invite URL", color=config.Success())
         embed.add_field(name="Invite me by clicking the link below", value=f"Invite me by clicking [here](https://discord.com/api/oauth2/authorize?client_id=1041164439199694868&permissions=8&scope=bot)", inline=True)
         await inter.author.send(embed=embed)
         await inter.response.send_message("I sent you a private message!")
@@ -62,7 +62,11 @@ class general(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = self.bot.get_channel(config.welcome_channel)
-        embed = disnake.Embed(title=f"Welcome {member.name}!", description=f"Welcome to {member.guild.name}! We hope you enjoy your stay here!", color=disnake.Color.random())
+        embed = disnake.Embed(title=f"Welcome {member.name}!", description=f"Welcome to {member.guild.name}! We hope you enjoy your stay here!", color=config.Success())
+        embed.add_field (name="\nUser Info", value=f"\n**User:** ```{member.name}#{member.discriminator} ({member.id})```\n**Account Created:** ```{member.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n**Joined Server:** ```{member.joined_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```", inline=False)
+        embed.add_field (name="User Avatar", value=f"[Click Here]({member.avatar.url})", inline=False)
+        embed.add_field (name="User Profile", value=f"[Click Here](https://discord.com/users/{member.id})", inline=False)
+        embed.add_field (name="User Mention", value=f"{member.mention}", inline=False)
         embed.set_thumbnail(url=member.avatar.url)
         embed.set_footer(text=f"{member.guild.name} | {member.guild.member_count} Members", icon_url=member.guild.icon.url)
         await channel.send(embed=embed)
@@ -72,7 +76,11 @@ class general(commands.Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = self.bot.get_channel(config.welcome_channel)
-        embed = disnake.Embed(title=f"Goodbye {member.name}!", description=f"Goodbye {member.name}! We hope you enjoyed your stay here!", color=disnake.Color.random())
+        embed = disnake.Embed(title=f"Goodbye {member.name}!", description=f"Goodbye {member.name}! We hope you enjoyed your stay here!", color=config.Error())
+        embed.add_field (name="\nUser Info", value=f"\n**User:** ```{member.name}#{member.discriminator} ({member.id})```\n**Account Created:** ```{member.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n**Joined Server:** ```{member.joined_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```", inline=False)
+        embed.add_field (name="User Avatar", value=f"[Click Here]({member.avatar.url})", inline=False)
+        embed.add_field (name="User Profile", value=f"[Click Here](https://discord.com/users/{member.id})", inline=False)
+        embed.add_field (name="User Mention", value=f"{member.mention}", inline=False)
         embed.set_thumbnail(url=member.avatar.url)
         embed.set_footer(text=f"{member.guild.name} | {member.guild.member_count} Members", icon_url=member.guild.icon.url)
         await channel.send(embed=embed)
