59775ef63cafefa3544069ecfb492625c792e84e
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-17 20:44:45 -0600

    Ban, Nuke, Kick Commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -99,5 +99,86 @@ class moderation(commands.Cog):
         embed.set_footer(text=f'Purged by {inter.author}', icon_url=inter.author.avatar.url)
         await inter.response.send_message(embed=embed)
 
+    # kick command 
+    @commands.slash_command(name='kick',
+                            description='Kick a member',)
+    async def kick(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member, *, reason: str = "No reason provided"):
+        if not inter.author.guild_permissions.kick_members:
+            embed = disnake.Embed(title=f"You do not have permission to kick ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(ephemeral=True, embed=embed)   
+        if not inter.guild.me.guild_permissions.kick_members:
+            embed = disnake.Embed(title=f"I do not have permission to kick ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(delete_after=15, embed=embed)
+        embed = disnake.Embed(title=f"You Have Been Kicked from **{member.guild.name}**!", color=config.Success())
+        embed.add_field(name="Reason:", value=f"``{reason}``", inline=False)
+        embed.set_footer(text=f'Kicked by {inter.author}', icon_url=inter.author.avatar.url)
+        await member.send(embed=embed)
+        await member.kick(reason=reason)
+        embed = disnake.Embed(title=f"Successfully Kicked ``{member}`` for ``{reason}``", color=config.Success())
+        embed.set_footer(text=f'Kicked by {inter.author}', icon_url=inter.author.avatar.url)
+        await inter.response.send_message(embed=embed)
+
+    # ban command 
+    @commands.slash_command(name='ban',
+                            description='Ban a member',)
+    async def ban(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member, *, reason: str = "No reason provided"):
+        if not inter.author.guild_permissions.ban_members:
+            embed = disnake.Embed(title=f"You do not have permission to ban ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(ephemeral=True, embed=embed)   
+        if not inter.guild.me.guild_permissions.ban_members:
+            embed = disnake.Embed(title=f"I do not have permission to ban ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(delete_after=15, embed=embed)
+        embed = disnake.Embed(title=f"You Have Been Banned from **{member.guild.name}**!", color=config.Success())
+        embed.add_field(name="Reason:", value=f"``{reason}``", inline=False)
+        embed.set_footer(text=f'Banned by {inter.author}', icon_url=inter.author.avatar.url)
+        await member.send(embed=embed)
+        await member.ban(reason=reason)
+        embed = disnake.Embed(title=f"Successfully Banned ``{member}`` for ``{reason}``", color=config.Success())
+        embed.set_footer(text=f'Banned by {inter.author}', icon_url=inter.author.avatar.url)
+        await inter.response.send_message(embed=embed)
+
+    # unban command 
+    @commands.slash_command(name='unban',
+                            description='Unban a member',)
+    async def unban(self, inter: disnake.ApplicationCommandInteraction, member: disnake.User):
+        if not inter.author.guild_permissions.ban_members:
+            embed = disnake.Embed(title=f"You do not have permission to unban ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(ephemeral=True, embed=embed)   
+        if not inter.guild.me.guild_permissions.ban_members:
+            embed = disnake.Embed(title=f"I do not have permission to unban ``{member}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(delete_after=15, embed=embed)
+        await inter.guild.unban(member)
+        embed = disnake.Embed(title=f"Successfully Unbanned ``{member}``", color=config.Success())
+        embed.set_footer(text=f'Unbanned by {inter.author}', icon_url=inter.author.avatar.url)
+        await inter.response.send_message(embed=embed)
+
+    # Nuke command that nukes channels 
+    @commands.slash_command(name='nuke',
+                            description='Nuke a channel',)
+    async def nuke(self, inter: disnake.ApplicationCommandInteraction, channel: disnake.TextChannel = None):
+        if not inter.author.guild_permissions.manage_channels:
+            embed = disnake.Embed(title=f"You do not have permission to nuke ``{channel}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(ephemeral=True, embed=embed)   
+        if not inter.guild.me.guild_permissions.manage_channels:
+            embed = disnake.Embed(title=f"I do not have permission to nuke ``{channel}!``", color=config.Error())
+            embed.set_footer(text=f'Attempted by {inter.author}', icon_url=inter.author.avatar.url)
+            return await inter.response.send_message(delete_after=15, embed=embed)
+        if channel is None:
+            channel = inter.channel
+        position = channel.position
+        new_channel = await channel.clone(reason=None)
+        await channel.delete()
+        await new_channel.edit(position=position)
+        embed = disnake.Embed(title=f"Successfully Nuked ``{channel}``", color=config.Success())
+        embed.set_footer(text=f'Nuked by {inter.author}', icon_url=inter.author.avatar.url)
+        await inter.response.send_message(embed=embed)
+
 def setup(bot):
     bot.add_cog(moderation(bot))
