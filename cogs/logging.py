95190de1a894453986f66511d134286b06a75d85
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-18 21:00:39 -0600

    Create logging.py

---
+++ cogs/logging.py
@@ -0,0 +1,27 @@
+###############################################
+#           Template made by Person0z         #
+#          https://github.com/Person0z        #
+#           Copyright© Person0z, 2022         #
+#           Do Not Remove This Header         #
+###############################################
+
+# imports and stuff
+import disnake
+from disnake.ext import commands
+import os
+import random
+
+class logging(commands.Cog):
+    def __init__(self, bot):
+    	self.bot = bot
+        
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Loaded Cog Logging')
+
+
+    # Coming Soon
+
+
+def setup(bot):
+    bot.add_cog(logging(bot))
