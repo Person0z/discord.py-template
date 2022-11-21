d2a74cafb3a24e709a6251d93ff3edbbd0f37eee
Hugo <hugo@zerbaib.pc>
2022-11-21 14:00:26 +0100

    i have to test

--- cogs/level.py
+++ cogs/level.py
@@ -1,4 +1,5 @@
 import disnake
+from disnake.ext import commands
 import configlvl
 from discordLevelingSystem import DiscordLevelingSystem, LevelUpAnnouncement, RoleAward
 
@@ -8,10 +9,13 @@ class level(commands.Cogs):
     
     @commands.slash_command()
     async def rank(ctx):
-        data = await lvl.get_data_for(ctx.author)
+        data = await configlvl.lvl.get_data_for(ctx.author)
         await ctx.send(f'You are level {data.level} and your rank is {data.rank}')
 
     @commands.slash_command()
     async def leaderboard(ctx):
-        data = await lvl.each_member_data(ctx.guild, sort_by='rank')
+        data = await configlvl.lvl.each_member_data(ctx.guild, sort_by='rank')
         # show the leaderboard whichever way you'd like
+
+def setup(bot):
+    bot.add_cog(level(bot))
