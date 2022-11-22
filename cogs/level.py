ffdf76acc579aa1c65f7af0a1120055157aa8dd7
Hugo <hugo@zerbaib.pc>
2022-11-22 12:11:28 +0100

    1 error corect now a have 2 error

--- cogs/level.py
+++ cogs/level.py
@@ -2,19 +2,22 @@ import disnake
 from disnake.ext import commands
 import configlvl
 import config
-from main import bot
 from discordLevelingSystem import DiscordLevelingSystem, LevelUpAnnouncement, RoleAward
 
-class level(commands.Cogs):
+class level(commands.Cog):
     def __init__(self, bot):
     	self.bot = bot
     
-    @commands.slash_command()
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Loaded Cog Level')
+
+    @commands.slash_command(name='rank', description='A rank command to see your level')
     async def rank(ctx):
         data = await configlvl.lvl.get_data_for(ctx.author)
         await ctx.send(f'You are level {data.level} and your rank is {data.rank}')
 
-    @commands.slash_command()
+    @commands.slash_command(name='leaderboard', description='A command to see the top of rank')
     async def leaderboard(ctx):
         data = await configlvl.lvl.each_member_data(ctx.guild, sort_by='rank')
         # show the leaderboard whichever way you'd like
