7d14865c5cf6939180c7e3806fd12a2f8e20aac8
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-22 00:40:19 -0600

    Redid Tags

--- cogs/tag.py
+++ cogs/tag.py
@@ -7,60 +7,42 @@ class Tag(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Tag parent command
+    # Tag Command in tag/<tag_name>.json
+    @commands.command()
+    async def tag(self, ctx, tag_name):
+        with open(f"tag/{tag_name}.json", "r") as f:
+            tag = json.load(f)
+        await ctx.send(tag["content"])
+
+
+    # Tag Create Command
     @commands.slash_command()
-    async def tag(
-        inter: disnake.ApplicationCommandInteraction,
-        action: str = commands.Param(choices=["create", "delete", "edit", "info"]),
-        name: str = commands.Param(description="The name of the tag"),
-        content: str = commands.Param(description="The content of the tag"),
-    ):
-        # If the user selects create, it will create a tag
-        if action == "create":
-            # Checks if the tag already exists
-            if os.path.exists(f"tags/{name}.json"):
-                await inter.response.send_message("That tag already exists.")
-            else:
-                # Creates the tag
-                with open(f"tags/{name}.json", "w") as f:
-                    json.dump({"name": name, "content": content}, f)
-                await inter.response.send_message(f"Tag {name} has been created.")
-        # If the user selects delete, it will delete the tag
-        elif action == "delete":
-            # Checks if the tag exists
-            if os.path.exists(f"tags/{name}.json"):
-                # Deletes the tag
-                os.remove(f"tags/{name}.json")
-                await inter.response.send_message(f"Tag {name} has been deleted.")
-            else:
-                await inter.response.send_message("That tag doesn't exist.")
-        # If the user selects edit, it will edit the tag
-        elif action == "edit":
-            # Checks if the tag exists
-            if os.path.exists(f"tags/{name}.json"):
-                # Edits the tag
-                with open(f"tags/{name}.json", "w") as f:
-                    json.dump({"name": name, "content": content}, f)
-                await inter.response.send_message(f"Tag {name} has been edited.")
-            else:
-                await inter.response.send_message("That tag doesn't exist.")
-        # If the user selects info, it will show info about the tag
-        elif action == "info":
-            # Checks if the tag exists
-            if os.path.exists(f"tags/{name}.json"):
-                # Gets the tag info
-                with open(f"tags/{name}.json", "r") as f:
-                    tag = json.load(f)
-                await inter.response.send_message(
-                    f"Name: {tag['name']}\nContent: {tag['content']}"
-                )
-            else:
-                await inter.response.send_message("That tag doesn't exist.")
-                
+    async def tag_create(self, ctx, tag_name, *, content):
+        with open(f"tag/{tag_name}.json", "w") as f:
+            json.dump({"content": content}, f)
+        await ctx.send("Tag Created!")
 
+    # Tag Delete Command
+    @commands.slash_command()
+    async def tag_delete(self, ctx, tag_name):
+        os.remove(f"tag/{tag_name}.json")
+        await ctx.send("Tag Deleted!")
 
-                
+    # Tag Edit Command
+    @commands.slash_command()
+    async def tag_edit(self, ctx, tag_name, *, content):
+        with open(f"tag/{tag_name}.json", "w") as f:
+            json.dump({"content": content}, f)
+        await ctx.send("Tag Edited!")
 
+    # Tag List Command
+    @commands.slash_command()
+    async def tag_list(self, ctx):
+        tags = []
+        for file in os.listdir("tag"):
+            if file.endswith(".json"):
+                tags.append(file[:-5])
+        await ctx.send(f"Tags: {', '.join(tags)}")
         
 def setup(bot):
     bot.add_cog(Tag(bot))
