dcbdc1aba765781ab240bc69554321f63f728e43
Person0z <80363995+Person0z@users.noreply.github.com>
2022-11-22 21:41:58 -0600

    Tags

--- cogs/tag.py
+++ cogs/tag.py
@@ -7,42 +7,74 @@ class Tag(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Tag Command in tag/<tag_name>.json
-    @commands.command()
-    async def tag(self, ctx, tag_name):
-        with open(f"tag/{tag_name}.json", "r") as f:
-            tag = json.load(f)
-        await ctx.send(tag["content"])
+    # Tag view command 
+    @commands.slash_command(name="tag", description="View a tag")
+    async def tag(self, inter, tag: str):
+        with open("tags.json", "r") as f:
+            tags = json.load(f)
 
+        if tag not in tags:
+            return await inter.send("That tag does not exist!")
 
-    # Tag Create Command
-    @commands.slash_command()
-    async def tag_create(self, ctx, tag_name, *, content):
-        with open(f"tag/{tag_name}.json", "w") as f:
-            json.dump({"content": content}, f)
-        await ctx.send("Tag Created!")
+        embed = disnake.Embed(title=tag, description=tags[tag], color=disnake.Color.random())
+        await inter.send(embed=embed)
 
-    # Tag Delete Command
-    @commands.slash_command()
-    async def tag_delete(self, ctx, tag_name):
-        os.remove(f"tag/{tag_name}.json")
-        await ctx.send("Tag Deleted!")
+    # Tag add command
+    @commands.slash_command(name="addtag", description="Add a tag")
+    async def addtag(self, inter, tag: str, *, content: str):
+        with open("tags.json", "r") as f:
+            tags = json.load(f)
 
-    # Tag Edit Command
-    @commands.slash_command()
-    async def tag_edit(self, ctx, tag_name, *, content):
-        with open(f"tag/{tag_name}.json", "w") as f:
-            json.dump({"content": content}, f)
-        await ctx.send("Tag Edited!")
+        if tag in tags:
+            return await inter.send("That tag already exists!")
 
-    # Tag List Command
-    @commands.slash_command()
-    async def tag_list(self, ctx):
-        tags = []
-        for file in os.listdir("tag"):
-            if file.endswith(".json"):
-                tags.append(file[:-5])
-        await ctx.send(f"Tags: {', '.join(tags)}")
-        
+        tags[tag] = content
+
+        with open("tags.json", "w") as f:
+            json.dump(tags, f, indent=4)
+
+        await inter.send(f"Added tag {tag}")
+
+    # Tag remove command
+    @commands.slash_command(name="removetag", description="Remove a tag")
+    async def removetag(self, inter, tag: str):
+        with open("tags.json", "r") as f:
+            tags = json.load(f)
+
+        if tag not in tags:
+            return await inter.send("That tag does not exist!")
+
+        tags.pop(tag)
+
+        with open("tags.json", "w") as f:
+            json.dump(tags, f, indent=4)
+
+        await inter.send(f"Removed tag {tag}")
+
+    # Tag list command
+    @commands.slash_command(name="taglist", description="List all tags")
+    async def taglist(self, inter):
+        with open("tags.json", "r") as f:
+            tags = json.load(f)
+
+        embed = disnake.Embed(title="Tags", description=", ".join(tags.keys()), color=disnake.Color.random())
+        await inter.send(embed=embed)
+
+    # Tag edit command
+    @commands.slash_command(name="edittag", description="Edit a tag")
+    async def edittag(self, inter, tag: str, *, content: str):
+        with open("tags.json", "r") as f:
+            tags = json.load(f)
+
+        if tag not in tags:
+            return await inter.send("That tag does not exist!")
+
+        tags[tag] = content
+
+        with open("tags.json", "w") as f:
+            json.dump(tags, f, indent=4)
+
+        await inter.send(f"Edited tag {tag}")
+
 def setup(bot):
     bot.add_cog(Tag(bot))
