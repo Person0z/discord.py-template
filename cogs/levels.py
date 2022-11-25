3a31b6c9406cec564882c76bffeadff306dd19ea
Zerbaib <zerbaib@hugopcubuntu.home>
2022-11-25 19:27:41 +0100

    base of the level sys

---
+++ cogs/levels.py
@@ -0,0 +1,14 @@
+import disnake
+from disnake.ext import commands
+import discordLevelingSysteme
+
+class level(commands.Cogs):
+    def __init__(self, bot):
+    	self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Loaded Cog Levels')
+
+def setup(bot):
+    bot.add_cog(level())
