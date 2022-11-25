e76226f6e2fd62fc3c400d62e7340c6ccea9d2a6
Zerbaib <zerbaib@hugopcubuntu.home>
2022-11-25 19:41:24 +0100

    add other

--- exemple.config.py
+++ exemple.config.py
@@ -6,3 +6,9 @@ lvl.connect_to_database_file(r'db\DiscordLevelingSystem.db')
 
 nitro_booster = 851379776111116329
 associate_role = 851400453904400385
+lvlupchan = 00000000000000
+
+embed = discord.Embed()
+embed.set_author(name=LevelUpAnnouncement.Member.name, icon_url=LevelUpAnnouncement.Member.avatar_url)
+embed.description = f'Congrats {LevelUpAnnouncement.Member.mention}! You are now level {LevelUpAnnouncement.LEVEL} 😎'
+announcement = LevelUpAnnouncement(embed, level_up_channel_ids=[lvlupchan])
