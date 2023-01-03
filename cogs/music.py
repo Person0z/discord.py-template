cb7f59e43b6bdd11c398a66335b00bddb6736929
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2023-01-03 05:19:50 +0100

    on ready music

--- cogs/music.py
+++ cogs/music.py
@@ -5,5 +5,9 @@ class music(commands.Cog):
     def __init__(self, bot):
     	self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Loaded Cog Music')
+
 def setup(bot):
     bot.add_cog(music(bot))
