4a7fe53f2e2197f92862df2d925c268674984873
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2023-01-04 13:52:48 +0100

    play command create

--- cogs/music.py
+++ cogs/music.py
@@ -1,5 +1,6 @@
 import disnake
 from disnake.ext import commands
+import config
 
 class music(commands.Cog):
     def __init__(self, bot):
@@ -8,6 +9,12 @@ class music(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Loaded Cog Music')
+
+    @commands.slash_command(name="play", description="play music on youtube")
+    async def play(self, inter: disnake.ApplicationCommandInteraction, name: int):
+        embed = disnake.Embed(title=f"Successfully add music {name} to queue", color=config.Success())
+        embed.set_footer(text=f'Set by {inter.author}', icon_url=inter.author.avatar.url)
+        await inter.response.send_message(embed=embed)
 
 def setup(bot):
     bot.add_cog(music(bot))
