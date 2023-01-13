ad58b3c9ff24bfef2383e71ba007d0cf168076ec
Person0z <80363995+Person0z@users.noreply.github.com>
2023-01-13 13:52:30 -0600

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -18,7 +18,7 @@ guild = 'GUILD ID'
 prefix = '!'
 
 # Bot Status
-activity = ["/help", "discord.py", "With Python", "Made by Person0z", "V.1.3-beta", "Made with Zerbaib", "In dev"]
+activity = ["/help", "discord.py", "With Python", "Made by Person0z", "v1.5.1"]
 
 # Colors
 Success = disnake.Color.green
