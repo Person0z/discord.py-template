242057336789f88ea17b6eed4c1ec3ee67324b22
Hugo Hennetin <hugohennetin@zerbaib.git>
2023-01-27 17:11:02 +0100

    set the react

--- cogs/hello.py
+++ cogs/hello.py
@@ -20,7 +20,34 @@ class hello(commands.Cog):
     async def on_ready(self):
         print(f'Loaded Cog General')
 
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.content.startswith("hey"):
+            await message.add_reaction("👋")
 
+        if message.content.startswith("yo"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("hello"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("wlc"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("welcome"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("bonjour"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("salut"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("bjr"):
+            await message.add_reaction("👋")
+
+        if message.content.startswith("slt"):
+            await message.add_reaction("👋")
 
 def setup(bot):
     bot.add_cog(hello(bot))
