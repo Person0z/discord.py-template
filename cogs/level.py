87c64bc53ed1c41b972a933daea073669b5d401f
Hugo Hennetin <hugohennetin@zerbaib.git>
2023-01-29 15:41:25 +0100

    create a dificulty with the level with stage

--- cogs/level.py
+++ cogs/level.py
@@ -51,9 +51,26 @@ class level(commands.Cog):
         if calc_lvl < 1:
             lvl = 0
         lvl = round(calc_lvl)
+        if lvl == 0:
+            stg = 0
+        if 0 < lvl:
+            stg = 1
+        if 100 < lvl:
+            stg = 2
+        if 500 < lvl:
+            stg = 3
+        if 1500 < lvl:
+            stg = 4
+        if 3000 < lvl:
+            stg = 5
+        if 5000 < lvl:
+            stg = 6
+        if 10000 < lvl:
+            stg = "🌟"
         embedVar = disnake.Embed(colour=config.Success())
         embedVar.add_field(name="You have", value=f"**``{xp}`` xp**", inline=False)
         embedVar.add_field(name="You are at the", value=f"**``{lvl}`` level**", inline=False)
+        embedVar.add_field(name="You are at the", value=f"**``{stg}`` stage**", inline=False)
         await inter.response.send_message(embed=embedVar)
 
     @commands.slash_command(name="xp", description="Check your total xp!")
@@ -76,6 +93,36 @@ class level(commands.Cog):
         lvl = round(calc_lvl)
         embedVar = disnake.Embed(colour=config.Success())
         embedVar.add_field(name="You are at the", value=f"**``{lvl}`` level**", inline=False)
+        await inter.response.send_message(embed=embedVar)
+
+    @commands.slash_command(name="stage", description="Check your stage")
+    async def stage(self, inter):
+        with open('./rank.json') as rank_file:
+            data = json.load(rank_file)
+        xp = data.get(f"{inter.author.id}")
+        calc_lvl = xp/100
+        if calc_lvl < 1:
+            lvl = 0
+        lvl = round(calc_lvl)
+
+        if lvl == 0:
+            stg = 0
+        if 0 < lvl:
+            stg = 1
+        if 100 < lvl:
+            stg = 2
+        if 500 < lvl:
+            stg = 3
+        if 1500 < lvl:
+            stg = 4
+        if 3000 < lvl:
+            stg = 5
+        if 5000 < lvl:
+            stg = 6
+        if 10000 < lvl:
+            stg = "🌟"
+        embedVar = disnake.Embed(colour=config.Success())
+        embedVar.add_field(name="You are at the", value=f"**``{stg}`` stage**", inline=False)
         await inter.response.send_message(embed=embedVar)
 
 def setup(bot):
