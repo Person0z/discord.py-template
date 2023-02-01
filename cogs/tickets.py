6f31bc253b603ae9a1b95e612facf52fb379c54d
Person0z <80363995+Person0z@users.noreply.github.com>
2023-02-01 02:23:33 -0600

    im getting very pissed cuz of this

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -21,21 +21,13 @@ class tickets(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'Loaded Cog Tickets')
-            
-
-#    @commands.slash_command()
-#    async def blep(
-#        inter: disnake.ApplicationCommandInteraction,
-#        animal: str = commands.Param(choices=["Dog", "Cat", "Penguin"])
-#    ):
-#        await inter.response.send_message(animal)
 
     # Slash command for tickets which has options of opening a ticket, closing a ticket, and deleting a ticket
     @commands.slash_command()
     async def ticket(
         inter: disnake.ApplicationCommandInteraction,
-        action: str = commands.Param(choices=["open", "close", "add"]),
-        user: disnake.User = commands.Param(description="The user you want to add to the ticket")
+        action: str = commands.Param(choices=["open", "close", "add", "remove"]),
+        user: disnake.User = commands.Param(None, description="The user you want to add to the ticket")
     ):
         try:
             # If the user selects open, it will create a ticket
@@ -43,12 +35,18 @@ class tickets(commands.Cog):
                 # Creates a ticket category if it doesn't exist
                 if not disnake.utils.get(inter.guild.categories, name="Tickets"):
                     await inter.guild.create_category("Tickets")
+                  
+                # Checks if the user already has a ticket open
+                if disnake.utils.get(inter.guild.channels, name=f"ticket-{inter.author.name}"):
+                    await inter.response.send_message("You already have a ticket open.")
+                    return
+
                 # Creates the ticket channel
                 channel = await inter.guild.create_text_channel(
                     name=f"ticket-{inter.author.name}",
                     category=disnake.utils.get(inter.guild.categories, name="Tickets"),
                     topic=f"Ticket for {inter.author.name}",
-                    reason=f"Ticket created by {inter.author.name}",
+                    reason=f"Ticket created by {inter.author.name} | {inter.author.id} Time Created: {inter.created_at}",
                 )
                 # Sets the permissions for the ticket channel
                 await channel.set_permissions(inter.author, send_messages=True, read_messages=True)
@@ -58,10 +56,12 @@ class tickets(commands.Cog):
                 await channel.send(
                     f"Hello {inter.author.mention}, welcome to your ticket! Please wait for a staff member to help you."
                 )
-                # Sends a message to the user
-                await inter.response.send_message(
-                    f"Your ticket has been created at {channel.mention}"
-                )
+                # Sends a message to the user, as an embed
+                embed = disnake.Embed(title="Ticket Created", description=f"Your ticket has been created at {channel.mention} \n Please go to your ticket and list your issues / concerns", color=0x00FF00)
+                embed.set_footer(text=f"Thank you for using {inter.guild.name}!")
+                embed.set_thumbnail(url=inter.author.avatar.url)
+                await inter.response.send_message(embed=embed, ephemeral=True)
+
             # If the user selects close, it will close the ticket
             elif action == "close":
                 channel = disnake.utils.get(inter.guild.channels, name=f"ticket-{inter.author.name}")
@@ -72,15 +72,20 @@ class tickets(commands.Cog):
                     await inter.response.send_message("You don't have a ticket open.")
             # If the user selects add, it will add a user to the ticket Ex: /ticket action:add user:Person0z
             elif action == "add":
-                channel = disnake.utils.get(inter.guild.channels, name=f"ticket-{inter.author.name}")
                 if inter.channel.name.startswith("ticket-"):
-                    await channel.set_permissions(inter.user, send_messages=True, read_messages=True)
+                    await inter.channel.set_permissions(user, send_messages=True, read_messages=True)
                     await inter.response.send_message(f"{user.mention} has been added to the ticket.")
                 else:
                     await inter.response.send_message("You don't have a ticket open.")
+            elif action == "remove":
+                if inter.channel.name.startswith("ticket-"):
+                    await inter.channel.set_permissions(user, send_messages=False, read_messages=False)
+                    await inter.response.send_message(f"{user.mention} has been removed from the ticket.")
+                else:
+                    await inter.response.send_message("You don't have a ticket open.")
+
         except Exception as e:
-            print(f'Error in ticket: {e}')
-            await inter.send(embed=errors.create_error_embed(f"Error sending ticket command: {e}"))
+            await inter.response.send_message(f"An error has occured: {e}")
 
 def setup(bot):
     bot.add_cog(tickets(bot))
