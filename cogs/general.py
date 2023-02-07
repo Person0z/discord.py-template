f8c2e0f80c22fa4d2340d05da97a591653144041
Zerbaib <hugo.hennetin@proton.me>
2023-02-07 16:51:29 +0100

    merge

--- cogs/general.py
+++ cogs/general.py
@@ -26,7 +26,8 @@ class general(commands.Cog):
         print(f'Loaded Cog General')
 
     # Ping Command
-    @commands.slash_command(name='ping', description='Get the bot\'s latency')
+    @commands.slash_command(name='ping',
+                            description='Get the bot\'s latency',)
     async def ping(self, inter: disnake.ApplicationCommandInteraction):
         try:
             embed = disnake.Embed(title=f"Pong!", description=f"The ping is around `{round(self.bot.latency * 1000)}ms`", color=config.Success())
@@ -47,8 +48,44 @@ class general(commands.Cog):
             print(f'Error Sending Check Command: {e}')
             await inter.send(embed=errors.create_error_embed(f"Error sending check command: {e}"))
 
+    # a welcome message when someone joins the server with there porfile picture and name in the embed and a welcome message in the chat in a custom channel
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        try:
+            channel = self.bot.get_channel(config.welcome_channel)
+            role = disnake.utils.get(member.guild.roles, name=config.join_role)
+            if role:
+                try:
+                    await member.add_roles(role)
+                except Exception as e:
+                    print(f'Error adding role to member: {e}')
+            embed = disnake.Embed(title=f"Welcome {member.name}!", description=f"{member.guild.name}! We hope you enjoy your stay here!", color=config.Success())
+            embed.add_field (name="\nUser Info", value=f"\n**User:** \n```{member.name}#{member.discriminator} ({member.id})```\n**Account Created:** \n```{member.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n**Joined Server:** \n```{member.joined_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n", inline=False)
+            embed.set_thumbnail(url=member.avatar.url)
+            embed.set_footer(text=f"{member.guild.name} | {member.guild.member_count} Members", icon_url=member.guild.icon.url)
+            await channel.send(embed=embed)
+            await channel.send(f"{member.mention}", delete_after=0.2)
+        except Exception as e:
+            print(f'Error sending welcome message: {e}')
+
+    # a goodbye message when someone leaves the server with there porfile picture and name in the embed and a goodbye message in the chat in a custom channel
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        try:
+            channel = self.bot.get_channel(config.welcome_channel)
+            embed = disnake.Embed(title=f"Goodbye {member.name}!", description=f"Goodbye {member.name}! We hope you enjoyed your stay here!", color=config.Error())
+            embed.add_field (name="\nUser Info", value=f"\n**User:** ```{member.name}#{member.discriminator} ({member.id})```\n**Account Created:** ```{member.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n**Joined Server:** ```{member.joined_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n", inline=False)
+            embed.set_thumbnail(url=member.avatar.url)
+            embed.set_footer(text=f"{member.guild.name} | {member.guild.member_count} Members", icon_url=member.guild.icon.url)
+            await channel.send(embed=embed)
+        except Exception as e:
+            print(f'Error sending goodbye message: {e}')
+
+
     # user info command
-    @commands.slash_command(name='userinfo', description='Get info about a user')
+    @commands.slash_command(name='userinfo',
+                            description='Get info about a user',)
     async def userinfo(self, inter: disnake.ApplicationCommandInteraction, member: disnake.Member = None):
         try:
             if member is None:
@@ -71,7 +108,8 @@ class general(commands.Cog):
             await inter.send(embed=errors.create_error_embed(f"Error sending userinfo command: {e}"))
     
     # server info command
-    @commands.slash_command(name='serverinfo', description='Get info about the server')
+    @commands.slash_command(name='serverinfo',
+                            description='Get info about the server',)
     async def serverinfo(self, inter: disnake.ApplicationCommandInteraction):
         try:
             embed = disnake.Embed(title=f"{inter.guild.name}'s Info", color=config.Success())
@@ -86,7 +124,8 @@ class general(commands.Cog):
             await inter.send(embed=errors.create_error_embed(f"Error sending serverinfo command: {e}"))
         
     # Bot CPU and RAM usage with storage usage
-    @commands.slash_command(name='botinfo', description='Get info about the bot')
+    @commands.slash_command(name='botinfo',
+                            description='Get info about the bot',)
     async def botinfo(self, inter: disnake.ApplicationCommandInteraction):
         try:
             cpu = psutil.cpu_percent()
@@ -95,18 +134,9 @@ class general(commands.Cog):
             storage_used = psutil.disk_usage('/').used / (1024.0 ** 3)
             storage_total = psutil.disk_usage('/').total / (1024.0 ** 3)
             embed = disnake.Embed(title=f"{self.bot.user.name}'s Info", color=config.Success())
-            embed.add_field(name="**Bot:**", value=f"```{self.bot.user.name}#{self.bot.user.discriminator} ({self.bot.user.id})```", inline=False)
-            embed.add_field(name="**Bot Created:**", value=f"```{self.bot.user.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```", inline=False)
-            embed.add_field(name="**Bot CPU Usage:**", value=f"```{cpu}% / 100%```")
-            embed.add_field(name="**Bot RAM Usage:**", value=f"```{ram_used:.2f} GB / {ram_total:.2f} GB```")
-            embed.add_field(name="**Bot Storage Usage:**", value=f"```{storage_used:.2f} GB / {storage_total:.2f} GB```")
-            embed.add_field(name="**Bot Ping:**", value=f"```{round(self.bot.latency * 1000)}ms```")
-            embed.add_field(name="**Bot Version:**", value=f"```{config.version}```")
-            embed.add_field(name="**Bot Library:**", value=f"```Disnake```")
-            embed.add_field(name="**Bot Servers:**", value=f"```{len(self.bot.guilds)}```", inline=False)
-            embed.add_field(name="**Bot Developer:**", value=f"```Person0z#0812\n./Zerbaib.sh#6400\nJaymart95```", inline=False)
-            embed.add_field(name="Bot Avatar", value=f"[Click Here]({self.bot.user.avatar.url})", inline=False)
-            embed.add_field(name="Bot Profile", value=f"[Click Here](https://discord.com/users/{self.bot.user.id})", inline=False)
+            embed.add_field (name="\nBot Info", value=f"\n**Bot:** ```{self.bot.user.name}#{self.bot.user.discriminator} ({self.bot.user.id})```\n**Bot Created:** ```{self.bot.user.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}```\n**Bot CPU Usage:** ```{cpu}% / 100%```\n**Bot RAM Usage:** ```{ram_used:.2f} GB / {ram_total:.2f} GB```\n**Bot Storage Usage:** ```{storage_used:.2f} GB / {storage_total:.2f} GB```\n**Bot Ping:** ```{round(self.bot.latency * 1000)}ms```\n**Bot Version:** ```{config.version}```\n**Bot Library:** ```Disnake```\n**Bot Developer:** ```Person0z#0812```", inline=False)
+            embed.add_field (name="Bot Avatar", value=f"[Click Here]({self.bot.user.avatar.url})", inline=False)
+            embed.add_field (name="Bot Profile", value=f"[Click Here](https://discord.com/users/{self.bot.user.id})", inline=False)
             embed.set_thumbnail(url=self.bot.user.avatar.url)
             embed.set_footer(text=f'Requested by {inter.author}', icon_url=inter.author.avatar.url)
             await inter.response.send_message(embed=embed)
@@ -116,7 +146,8 @@ class general(commands.Cog):
 
 
     # invite the bot to your server
-    @commands.slash_command(name='invite', description='Invite the bot to your server')
+    @commands.slash_command(name='invite',
+                            description='Invite the bot to your server',)
     async def invite(self, inter: disnake.ApplicationCommandInteraction):
         try:
             embed = disnake.Embed(title="Invite Me", color=config.Success())
